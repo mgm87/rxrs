67b07b762ba16f3092b1e80c78e750d58556e554
MSHARE\mmadsen <mmadsen@inmoment.com>
2018-08-02 21:56:11 -0600

    initial commit

---
+++ test/rxrs.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/rxrs'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
