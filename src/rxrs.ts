49662e5ce01f1242de1a5ac461b25fe0e4458f04
MSHARE\mmadsen <mmadsen@inmoment.com>
2018-09-03 22:55:50 -0600

    first library commit

--- src/rxrs.ts
+++ src/rxrs.ts
@@ -1,4 +1,61 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export default class RxRs {
+  private topWindow: Window;
+  private windowSizeSubjects: SizeSubjects = {};
+
+  constructor() {
+    this.topWindow = this.getWindow();
+  }
+
+  observe(query: string): Observable<boolean> {
+    const querystring = this.uglifyQuery(query);
+    let subject = this.windowSizeSubjects[querystring];
+
+    if (!subject) {
+      const mql = this.topWindow.matchMedia(query);
+      this.windowSizeSubjects[querystring] = new BehaviorSubject(mql.matches);
+      mql.addListener(this.testQuery);
+      subject = this.windowSizeSubjects[querystring];
+    }
+
+    return subject.asObservable();
+  }
+
+  private uglifyQuery(query: string): string {
+    return query.toLowerCase().replace(/\s/g, '');
+  }
+
+  private testQuery(e: any): void {
+    const subject = this.windowSizeSubjects[this.uglifyQuery(e.media)];
+    if (subject) {
+      subject.next(e.matches);
+    }
+  }
+
+  private getWindow(): Window {
+    return window.top;
+  }
+
+  // var mql = window.matchMedia('(max-width: 600px)');
+
+  // function screenTest(e) {
+  //   if (e.matches) {
+  //     /* the viewport is 600 pixels wide or less */
+  //     console.log('This is a narrow screen — less than 600px wide.');
+  //   } else {
+  //     /* the viewport is more than than 600 pixels wide */
+  //     console.log('This is a wide screen — more than 600px wide.');
+  //   }
+  // }
+
+  // mql.addListener(screenTest);
+}
+
+interface SizeSubjects {
+  [key: string]: BehaviorSubject<boolean>;
+}
--- test/rxrs.test.ts
+++ test/rxrs.test.ts
@@ -1,14 +1,124 @@
-import DummyClass from '../src/rxrs'
+import { cold } from 'jasmine-marbles';
+import { of } from 'rxjs';
+
+import RxRs from '../src/rxrs';
 
 /**
- * Dummy test
+ * RxRs
  */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
+describe('RxRs', () => {
+  it('RxRs is instantiable', () => {
+    expect(new RxRs()).toBeInstanceOf(RxRs);
+  });
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+  describe('observe', () => {
+    it('should return an observable for the given query string', () => {
+      // Arrange
+      global.window.matchMedia = () => {
+        return {
+          matches: false,
+          addListener: () => {
+            return {};
+          }
+        };
+      };
+      const rxrs = new RxRs();
+      const queryString = '(max-width: 480px)';
+      const expected = cold('a', { a: false });
+
+      // Act
+      const actual = rxrs.observe(queryString);
+
+      // Assert
+      expect(actual).toBeObservable(expected);
+    });
+
+    it('should return the same observable for the given query string', () => {
+      // Arrange
+      let firstCall = true;
+      global.window.matchMedia = () => {
+        return {
+          matches: () => {
+            if (firstCall) {
+              firstCall = false;
+              return true;
+            } else {
+              return false;
+            }
+          },
+          addListener: () => {
+            return {};
+          }
+        };
+      };
+      const rxrs = new RxRs();
+      const queryString = '(max-width: 480px)';
+
+      // Act
+      const expected = rxrs.observe(queryString);
+      const actual = rxrs.observe(queryString);
+
+      // Assert
+      expect(actual).toEqual(expected);
+    });
+
+    it('should return a new observable for a new query string', () => {
+      // Arrange
+      let firstCall = true;
+      global.window.matchMedia = () => {
+        return {
+          matches: () => {
+            if (firstCall) {
+              firstCall = false;
+              return true;
+            } else {
+              return false;
+            }
+          },
+          addListener: () => {
+            return {};
+          }
+        };
+      };
+      const rxrs = new RxRs();
+      const queryString1 = '(max-width: 480px)';
+      const queryString2 = '(max-width: 360px)';
+
+      // Act
+      const expected = rxrs.observe(queryString1);
+      const actual = rxrs.observe(queryString2);
+
+      // Assert
+      expect(actual).not.toEqual(expected);
+    });
+  });
+
+  // describe('testQuery', () => {
+  //   it('should get next observable value when callback is called', () => {
+  //     // Arrange
+  //     let callbackFn: Function;
+  //     global.window.matchMedia = () => {
+  //       return {
+  //         matches: () => false,
+  //         addListener: (callback: Function) => {
+  //           callbackFn = callback;
+  //           return;
+  //         }
+  //       };
+  //     };
+  //     const rxrs = new RxRs();
+  //     const queryString = '(max-width: 480px)';
+  //     const actual = rxrs.observe(queryString);
+  //     const expected = cold('a', { a: true });
+
+  //     // Act
+  //     callbackFn({
+  //       media: queryString,
+  //       matches: true
+  //     });
+
+  //     // Assert
+  //     expect(actual).toBeObservable(expected);
+  //   });
+  // });
+});
